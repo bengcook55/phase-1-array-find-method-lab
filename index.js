9d0e7623fd654f78796311200fbafd5fb42b6661
Benjamin Cook <bengcook55@gmail.com>
2022-11-16 20:44:44 -0500

    commit tree 1f16e8786289b097b841dd75d920b2162d9fb628

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const { reconfigureWindow } = require("jsdom");
+
+
 // code your solution here
+
+function superbowlWin(record) {
+    const result = record.find((currentRecord) => {
+      return currentRecord.result === "W";
+    });
+    if (result) {
+      return result.year;
+    }
+    return undefined;
+  }
